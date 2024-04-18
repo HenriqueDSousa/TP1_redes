097fbafd10b80b2577d857fa5dee391475ec796b
gfjallais <gfjallais@gmail.com>
2024-04-18 00:37:22 -0300

    Mexendo em muita coisa

--- bridge_defense.py
+++ bridge_defense.py
@@ -2,10 +2,44 @@
 
 import socket
 import sys
-import json
-import struct
 from utils import *
 
+SHOTS_TO_SINK = {
+    'frigate': 1,
+    'destroyer': 2,
+    'battleship': 3
+}
+
+def get_shots_list(cannons_table, ships_table):
+    shots_list = []
+    for row in range(5):
+        for bridge in range(8):
+            if cannons_table[row][bridge] == 1:
+                weakest_ship = -1
+                river = -1
+                weakest_ship_life = 1000
+                if row < 4:
+                    if ships_table[row][bridge] != []:
+                        for ship in ships_table[row][bridge]:
+                            ship_life = SHOTS_TO_SINK[ship.get('hull')] - ship.get('hits')
+                            if ship_life < weakest_ship_life:
+                                weakest_ship = ship.get('id')
+                                river = row
+                                print([bridge + 1, row], weakest_ship)
+                                weakest_ship_life = ship_life
+                if row > 0:
+                    if ships_table[row-1][bridge] != []:
+                        for ship in ships_table[row-1][bridge]:
+                            ship_life = SHOTS_TO_SINK[ship.get('hull')] - ship.get('hits')
+                            if ship_life < weakest_ship_life:
+                                weakest_ship = ship.get('id')
+                                river = row - 1
+                                print([bridge + 1, row], weakest_ship)
+                                weakest_ship_life = ship_life
+                if weakest_ship != -1:
+                    temp_dict = {'cannon': [bridge + 1, row], 'id': weakest_ship, 'river': river+1}
+                    shots_list.append(temp_dict)
+    return shots_list
 
 def main():
 
@@ -18,36 +52,51 @@ def main():
     gas = sys.argv[3]  # Group Authentication Sequence (GAS)
 
     print(gas)
-    # List of server addresses and ports
-    servers = [
-        (host, port_offset + i) for i in range(4)
-    ]
-
     # Creating sockets for each server
-    sockets = [socket.socket(socket.AF_INET, socket.SOCK_DGRAM) for _ in range(4)]
+    servers = [{"address": (host, port_offset+i), "id": i, "socket": socket.socket(socket.AF_INET, socket.SOCK_DGRAM)} for i in range(4)]
+
+    for server in servers:
+        server.get('socket').settimeout(1)
+
     # AUTHENTICATION
-     
-    auth(gas, sockets, servers)
+    auth(gas, servers)
+    for server in servers:
+        print(server)
 
     # GETCANNONS
+    cannons = get_cannons(gas, servers)
+    print(cannons)
 
-    data = {
-            "type": "getcannons",
-            "auth": gas,
-        }
-    
-    json_data = json.dumps(data)
-    #sending to one server
-    send_to_servers(sockets, [servers[0]], json_data)
-    responses = receive_from_servers([sockets[0]])
+    cannons_table = [[0 for _ in range(8)] for _ in range(5)]
+    for coordinate in cannons:
+        cannons_table[coordinate[1]][coordinate[0]-1] = 1
+    for row in cannons_table:
+        print(row)
 
-    print(responses)    
+    # GETTURN
+    response = get_turn(gas, servers, 0)
 
+    ships_table = [[[] for _ in range(8)] for _ in range(4)]
+    for i, resp in enumerate(response):
+        print(resp)
+        bridge = resp.get('bridge')
+        ships = resp.get('ships')
+        ships_table[i][bridge-1] = ships
+    for row in ships_table:
+        print(row)
+    print("###########")
 
-    for sock in sockets:
-        sock.close()
+    shots_list = get_shots_list(cannons_table, ships_table)
 
-    
+    print("###########")
+    for row in shots_list:
+        print(row)
+    # SHOT
+    response = shot(gas, servers, shots_list)
+
+    for server in servers:
+        server.get("socket").close()
+
     return
 
 
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import socket
-import sys
 import json
-import struct
+import time
 
 
 def logexit(msg):
@@ -11,54 +10,163 @@ def logexit(msg):
 def usage():
     print("./bridge_defense.py <host> <port> <GAS>")
 
-def send_to_servers(sockets, servers, json_data):
+def send_to_servers(servers, json_data):
     """
     Function to send the same message to all servers
     """
-
-    for i, server_address in enumerate(servers):
-        
+    for server in servers:
+        address = server.get('address')
+        sock = server.get('socket')
         try:
-            sockets[i].sendto(json_data.encode(), server_address)
+            sock.sendto(json_data.encode(), address)
         except socket.error as e:
-            sockets[i].sendto(json_data.encode(), server_address)
+            sock.sendto(json_data.encode(), address)
 
-def receive_from_servers(sockets):
+def receive_from_servers(servers):
     """
     Function to get the responses from all servers
     """
-    responses = []
-    for i, sock in enumerate(sockets):
+    responses = [{}, {}, {}, {}]
+    for server in servers:
+        sock = server.get('socket')
+        id = server.get('id')
         try:
-            response, server_address = sock.recvfrom(1024)
+            response, _ = sock.recvfrom(1024)
             response = json.loads(response.decode())
-            responses.append(response)
+            responses[id] = response
         except socket.timeout:
-            response, server_address = sock.recvfrom(1024)
+            response, _ = sock.recvfrom(1024)
             response = json.loads(response.decode())
-            responses.append(response)
+            responses[id] = response
 
     return responses
 
-def auth(gas, sockets, servers):
+def get_empty_response(responses):
+    empty_responses = []
+    for i, response in enumerate(responses):
+        if len(response) == 0:
+            empty_responses.append(i)
+    return empty_responses
+
+def auth(gas, servers):
    
-    # sending the authentication to server
     data = {
             "type": "authreq",
             "auth": gas
         }
-        
-    json_data = json.dumps(data)
-    send_to_servers(sockets, servers, json_data)
     
-    # receiving authentication response from each server
-    responses = receive_from_servers(sockets)
-    print("sent")
-    # Checking authentication status
+    max_tries = 2
+    count = 0
+
+    json_data = json.dumps(data)
+    send_to_servers(servers, json_data)
+    responses = receive_from_servers(servers)
+    empty_responses = get_empty_response(responses)
+
+    while len(empty_responses) != 0 and count < max_tries:
+        temp_servers = [server for server in servers if server['id'] in empty_responses]
+        send_to_servers(temp_servers, json_data)
+        temp_response = receive_from_servers(temp_servers)
+        for i in empty_responses:
+            responses[i] = temp_response[i]
+        empty_responses = get_empty_response(responses)
+        count+=1
+
+    if len(get_empty_response(responses)) != 0:
+        logexit("Failed to authenticate with the server")
 
     for i, response in enumerate(responses):
-        print(response)
-
         if response.get('status', -1) != 0:
-            logexit("Autherror")
-   
+            logexit("Invalid GAS")
+        servers[i]['river'] = response.get('river')
+
+def get_cannons(gas, servers):
+    data = {
+            "type": "getcannons",
+            "auth": gas,
+        }
+    
+    responses = []
+    max_tries = 3
+    count = 0
+
+    json_data = json.dumps(data)
+
+    send_to_servers(servers, json_data)
+    responses = receive_from_servers(servers)
+    empty_responses = get_empty_response(responses)
+
+    while len(empty_responses) == 4 and count < max_tries:
+        send_to_servers(servers, json_data)
+        responses = receive_from_servers(servers)
+        empty_responses = get_empty_response(responses)
+        count+=1
+
+    if empty_responses == 4:
+        logexit("Failed to get cannons")
+
+    for response in responses:
+        if response != {}:
+            return response.get('cannons')
+
+def get_turn(gas, servers, turn):
+    data = {
+            "type": "getturn",
+            "auth": gas,
+            "turn": turn,
+        }
+
+    json_data = json.dumps(data)
+    max_tries = 3
+    count = 0
+
+    send_to_servers(servers, json_data)
+    responses = receive_from_servers(servers)
+    empty_responses = get_empty_response(responses)
+
+    while len(empty_responses) != 0 and count < max_tries:
+        temp_servers = [server for server in servers if server['id'] in empty_responses]
+        send_to_servers(temp_servers, json_data)
+        temp_response = receive_from_servers(temp_servers)
+        for i in empty_responses:
+            responses[i] = temp_response[i]
+        empty_responses = get_empty_response(responses)
+        count+=1
+    empty_responses = get_empty_response(responses)
+
+    if len(empty_responses) != 0:
+        logexit(f"Failed to get all turns from servers {empty_responses}")
+    return responses
+
+def shot(gas, servers, shots_list):
+    
+    for shot in shots_list:
+        cannon = shot.get("cannon")
+        server = shot.get("river") - 1
+        data = {
+                "type": "shot",
+                "auth": gas,
+                "cannon": cannon,
+                "id": shot.get("id")
+            }
+        
+        print(cannon, shot.get('id'))
+
+        max_tries = 10
+        count = 0
+        print(servers[server])
+        json_data = json.dumps(data)
+        send_to_servers([servers[server]], json_data)
+        server_id = servers[server].get('id')
+        responses = receive_from_servers([servers[server]])
+        while (len(responses[server_id]) == 0 or responses[server_id].get("type") != "shotresp") and count < max_tries:
+            time.sleep(2)
+            print(responses[server_id].get("type"))
+            send_to_servers([servers[server]], json_data)
+            responses = receive_from_servers([servers[server]])
+            count+=1
+
+        if (len(responses[server_id]) == 0 or responses[server_id].get("type") != "shotresp") and count < max_tries:
+            logexit(f"Failed to get shot response from server {server}")
+
+        print(responses[server_id])
